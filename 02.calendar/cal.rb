b2649d7f45f7a58ea0a8836bac3b7005e2fe032a
y.ikeda@genbasupport.com <ikeyou43@HW-GS-365.localdomain>
2026-07-29 10:24:50 +0900

    変数名・代入演算子前後のスペース・ループの書き方等の諸変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -18,36 +18,34 @@ opt.on('-m MONTH', Integer) { |v| options[:m] = v }
 #コマンドライン引数（ARGV）を解析
 opt.parse!(ARGV)
 #引数から年月を変数に代入
-year=options[:y]
-month=options[:m]
+year = options[:y]
+month = options[:m]
 
 #取得した年月を使ってその月の1日目の曜日を取得[日:0,月:1,...,土:6]
-day1=Date.new(year,month,1).wday
+first_day=Date.new(year,month,1).wday
 #取得した年月を使ってその月の最終日付を取得
-date_la=Date.new(year,month,-1).day
+last_date=Date.new(year,month,-1).day
 
-#カレンダーの年月と曜日を表示
+#カレンダーの年月と曜日を表示(漢字7文字+半角スペース6文字分の文字列)
 puts "#{month}月 #{year}".center(20)
 puts "日 月 火 水 木 金 土"
 #1日目の曜日までスペースで移動
 i=0
-day1.times do |i|
+first_day.times do |i|
   #漢字1文字+半角スペース1文字分開ける
   print "   "
 end
 #日付の記載を最終日まで繰り返す
-i=0
-d=day1
-while i<date_la
-  i=i+1
+d=first_day
+(1..last_date).each do |i|
   #土曜日が来るまでdを増やす 
-  d=d+1
+  d+=1
   if i<10
     print " "
   end
   print "#{i} "
   #土曜日が来たら改行し、nをリセット
-  if d%7==0
+  if (first_day+i)%7==0
     puts ""
     d=0
   end
