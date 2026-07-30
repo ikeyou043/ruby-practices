e975a99fe5117e2036559a0e21e676b2550eea71
y.ikeda@genbasupport.com <ikeyou43@HW-GS-365.localdomain>
2026-07-30 11:55:33 +0900

    ストライク時・スペア時の計算方法を旧ルールに変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -12,17 +12,41 @@ scores.each do |s|
     shots << s.to_i
   end
 end
-
+puts "入力スコア:#{shots}"
 frames = shots.each_slice(2).to_a
-
+puts "フレームスコア:#{frames}"
+# ストライク状態とスペア状態の記憶フラッグ
+strike_frag = 0
+spare_frag = 0
+# 現在のフレーム数を記録
+i = 0
 point = frames.sum do |frame|
-  if frame[0] == 10 # strike
-    30
-  elsif frame.sum == 10 # spare
-    frame[0] + 10
+  i += 1
+  puts "第#{i}フレーム:#{frame}"
+  if strike_frag == 1
+    print 'ストライク'
+    frame_score = frame.sum * 2
+
+  elsif spare_frag == 1
+    print 'スペア'
+    frame_score = frame[0] * 2 + frame[1]
+
   else
-    frame.sum
+    print '-'
+    frame_score = frame.sum
   end
+  puts "=>#{frame_score}点"
+  if frame[0] == 10 # strike
+    strike_frag = 1
+    spare_frag = 0
+  elsif frame.sum == 10 # spare
+    strike_frag = 0
+    spare_frag = 1
+  else
+    strike_frag = 0
+    spare_frag = 0
+  end
+  frame_score
 end
-p frames
+
 puts point
