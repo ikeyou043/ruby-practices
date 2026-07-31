4d1b969ee45692a60957eb85bf076c0ea0941ff4
y.ikeda@genbasupport.com <ikeyou43@HW-GS-365.localdomain>
2026-07-31 10:14:39 +0900

    デバッグのためのprint,putsを削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -6,24 +6,19 @@ scores = score.split(',')
 shots = scores.map do |s|
   s == 'X' ? 10 : s.to_i
 end
-puts "入力スコア:#{shots}"
 
 point = 0
 pos = 0
-10.times do |i|
-  print "第#{i + 1}フレーム "
+10.times do
   if shots[pos] == 10
-    puts "#{shots[pos]} , - ストライク"
     point += 10 + shots[pos + 1] + shots[pos + 2]
     pos += 1
   elsif (shots[pos] + shots[pos + 1]) == 10
-    puts "#{shots[pos]} , #{shots[pos + 1]} スペア"
     point += 10 + shots [pos + 2]
     pos += 2
   else
-    puts "#{shots[pos]} , #{shots[pos + 1]}"
     point += shots[pos] + shots[pos + 1]
     pos += 2
   end
 end
-puts "合計ポイント:#{point}"
+puts point
