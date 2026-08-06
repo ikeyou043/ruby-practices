4a7892565b81dcc2d88845c14b10a54ab3adc86d
y.ikeda@genbasupport.com <ikeyou43@HW-GS-365.localdomain>
2026-08-06 09:41:11 +0900

    =前後のスペースの追加や分かりにくいコメントの削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,17 +14,16 @@ opt = OptionParser.new
 #オプションの定義
 opt.on('-y YEAR', Integer) { |v| options[:y] = v }
 opt.on('-m MONTH', Integer) { |v| options[:m] = v }
-
-#コマンドライン引数（ARGV）を解析
 opt.parse!(ARGV)
+
 #引数から年月を変数に代入
 year = options[:y]
 month = options[:m]
 
 #取得した年月を使ってその月の1日目の曜日を取得
-first_date=Date.new(year,month,1)
+first_date = Date.new(year,month,1)
 #取得した年月を使ってその月の最終日付を取得
-last_date=Date.new(year,month,-1)
+last_date = Date.new(year,month,-1)
 
 #カレンダーの年月と曜日を表示(漢字7文字+半角スペース6文字分の文字列)
 puts "#{month}月 #{year}".center(20)
@@ -35,7 +34,7 @@ first_date.wday.times do
   print "   "
 end
 (first_date..last_date).each do |date|
-  # rjust(2) で桁数を揃え、後ろにスペースを1つ付与
+  # 空白込みで2桁になるように揃える
   print date.day.to_s.rjust(2) + " "
   # 土曜日が来たら改行する
   puts if date.saturday?
