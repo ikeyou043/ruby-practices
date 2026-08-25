ecab33b5272aaa1e3baa9932e873be4a97a0e547
y.ikeda@genbasupport.com <ikeyou43@HW-GS-365.localdomain>
2026-08-25 09:35:03 +0900

    空白文字の処理を修整

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
+
 MAX_COLUMNS = 3
-MARGIN_WIDTH = 3
+MARGIN_WIDTH = 2
+
 def format_in_columns(files, max_columns = MAX_COLUMNS)
   return '' if files.empty?
 
   row_count = files.size.fdiv(max_columns).ceil
-  sliced_files = files.each_slice(row_count).map { |slices| slices.values_at(0...row_count) }
-  rows = sliced_files.transpose
-
-  max_width = files.map(&:size).max
+  columns = files.each_slice(row_count).map { |slice| slice.values_at(0...row_count) }
+  col_widths = columns.map { |col| col.compact.map(&:size).max || 0 }
+  rows = columns.transpose
 
   formatted_lines = rows.map do |row|
-    row.map { |file| file.to_s.ljust(max_width + MARGIN_WIDTH) }.join.rstrip
+    row.map.with_index do |file, col_idx|
+      next if file.nil?
+
+      file.to_s.ljust(col_widths[col_idx] + MARGIN_WIDTH)
+    end.join.rstrip
   end
 
   formatted_lines.join("\n")
 end
 
 def main
-  # ファイル一覧を取得するメソッド
   files = Dir.glob('*').sort
   puts format_in_columns(files)
 end
