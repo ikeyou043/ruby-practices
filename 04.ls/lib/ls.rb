638858740ee541d5d5132b58ca5daa440d462247
y.ikeda@genbasupport.com <ikeyou43@HW-GS-365.localdomain>
2026-08-25 09:54:09 +0900

    rubocopパスのためのメソッド簡易化

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -8,18 +8,16 @@ def format_in_columns(files, max_columns = MAX_COLUMNS)
 
   row_count = files.size.fdiv(max_columns).ceil
   columns = files.each_slice(row_count).map { |slice| slice.values_at(0...row_count) }
-  col_widths = columns.map { |col| col.compact.map(&:size).max || 0 }
+  col_widths = columns.map { |col| col.compact.map(&:size).max }
   rows = columns.transpose
 
-  formatted_lines = rows.map do |row|
-    row.map.with_index do |file, col_idx|
-      next if file.nil?
+  rows.map { |row| format_row(row, col_widths) }.join("\n")
+end
 
-      file.to_s.ljust(col_widths[col_idx] + MARGIN_WIDTH)
-    end.join.rstrip
-  end
-
-  formatted_lines.join("\n")
+def format_row(row, col_widths)
+  row.map.with_index do |file, col_idx|
+    file&.to_s&.ljust(col_widths[col_idx] + MARGIN_WIDTH)
+  end.join.rstrip
 end
 
 def main
