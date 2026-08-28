ed99cee15d97208999cf453a852a31a06a34ce09
y.ikeda@genbasupport.com <ikeyou43@HW-GS-365.localdomain>
2026-08-28 10:24:14 +0900

    rオプションを追加

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
 
 require 'optparse'
+
 MAX_COLUMNS = 3
-MARGIN_WIDTH = 2
+MARGIN_WIDTH = 3
 
 def parse_options
   options = {}
   opt = OptionParser.new
-  opt.on('-a') { |v| options[:a] = v }
+  opt.on('-r') { |v| options[:r] = v }
   opt.parse!(ARGV)
   options
-end
-
-def fetch_files(options)
-  options[:a] ? Dir.entries('.') : Dir.glob('*')
 end
 
 def format_in_columns(files, max_columns = MAX_COLUMNS)
@@ -21,7 +18,7 @@ def format_in_columns(files, max_columns = MAX_COLUMNS)
 
   row_count = files.size.fdiv(max_columns).ceil
   columns = files.each_slice(row_count).map { |slice| slice.values_at(0...row_count) }
-  col_widths = columns.map { |col| col.compact.map(&:size).max }
+  col_widths = columns.map { |col| col.compact.map(&:size).max || 0 }
   rows = columns.transpose
 
   rows.map { |row| format_row(row, col_widths) }.join("\n")
@@ -35,7 +32,8 @@ end
 
 def main
   options = parse_options
-  files = fetch_files(options).sort
+  files = Dir.glob('*').sort
+  files = files.reverse if options[:r] # -r オプション指定時のみ反転
   puts format_in_columns(files)
 end
 
